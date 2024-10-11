5fc69c22eeaca3c95c196bdf55f4b8bdfefa5027
bernardo.souza <bernardo.souza@cwi.com.br>
2024-10-10 21:50:28 -0300

    show user answers

--- src/components/words/words.components.jsx
+++ src/components/words/words.components.jsx
@@ -7,10 +7,11 @@ export function Words({ language }) {
     const { words } = MOCK_DATA
     const [current, setCurrent] = useState(0)
     const [currentWord, setCurrentWord] = useState(words[current])
+    const [currentKey, setCurrentKey] = useState(0)
     const [userWord, setUserWord] = useState("")
+    const [history, setHistory] = useState({})
 
     const operations = {
-        ' ': nextWord,
         undefined: (key) => console.log('oi'),
         'Backspace': deleteWord,
         'regex': handleUserWord,
@@ -19,7 +20,7 @@ export function Words({ language }) {
     useEffect(() => {
         function handleKeyDown({key}) {
             if('Backspace' === key) operations[key]()
-            else if(/[a-zA-Z]/g.test(key) && 'Backspace' !== key) operations['regex'](key)
+            else if((/[a-zA-Z]/g.test(key) || ' ' === key || '-' === key) && key.length === 1) operations['regex'](key)
             else operations[key](key)
         }
 
@@ -31,41 +32,89 @@ export function Words({ language }) {
     }, [current, userWord])
     
     function nextWord() {
-        if (current + 1 < words.length) {
-            const next = words[current + 1]
-            setCurrent(current + 1)
-            setCurrentWord(next)
-        }
+        const next = words[current + 1]
+        setCurrent(current + 1)
+        setCurrentWord(next)
+        setUserWord("")
+        setHistory(old => ({...old, [current]: userWord}))
     }
 
     function deleteWord() {
-        console.log(userWord)
         if(userWord === "") {
             const previous = current !== 0 ? current - 1 : current
             setCurrent(previous)
             setCurrentWord(words[previous])
+            setCurrentKey(0)
         } else {
             setUserWord(old => old.slice(0, -1))
         }
     }
 
     function handleUserWord(key) {
-        setUserWord(old => `${old}${key}`)
+        if(userWord.length === currentWord.length) {
+            nextWord()
+            setCurrentKey(0)
+        } else {
+            setCurrentKey(old => old + 1)
+            setUserWord(old => `${old}${key}`)
+        }
     }
 
-    function isCurrentWord(word) {
-        return word.toLowerCase() === currentWord.toLowerCase()
+    function isCurrentWord(index) {
+        return current === index
+    }
+
+    function checkAnswer(keyIndex, key, wordIndex, word) {
+        if (current <= wordIndex) return ""
+        if (word[keyIndex] === key) return "ok"
+        return 'wrong'
     }
 
     return ( 
         <section>
             {userWord}
+            {currentKey}
             <ul className="words">
             {
+                // current + userWord.lenght => current lettle
                 words.map((word, index) => 
-                    <li key={index} className={`word current-${isCurrentWord(word)}`}>
-                        {word}
-                    </li>
+                    {
+                        if(history[index]) {
+                            return <li key={index} className="word">
+                            {
+                                word.split("")
+                                    .map((key, keyIndex) => {
+                                        if(isCurrentWord(index)) {
+                                            return <span key={keyIndex}
+                                                className={`key-${checkAnswer(keyIndex, key, index, history[index])}`}>{key}</span>
+                                        }
+                                        return <span 
+                                                    key={keyIndex} 
+                                                    className={`key-${checkAnswer(keyIndex, key, index, history[index])}`}>
+                                                    {key}
+                                                </span>
+                                    })
+                            }
+                            </li>
+                        }
+
+                        return <li key={index} className={`word current-${isCurrentWord(index)}`}>
+                        {
+                            word.split("")
+                                .map((key, keyIndex) => {
+                                    if(isCurrentWord(index)) {
+                                        return <span key={keyIndex}
+                                            className={`key-${checkAnswer(keyIndex, key, index, userWord)}`}>{key}</span>
+                                    }
+                                    return <span 
+                                                key={keyIndex} 
+                                                className={`key-${checkAnswer(keyIndex, key, index, userWord)}`}>
+                                                {key}
+                                            </span>
+                                })
+                        }
+                        </li>
+                    }
                 )     
             }
             </ul>
